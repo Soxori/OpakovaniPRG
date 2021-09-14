e17be40201129fbd292262ffde52a605e50b0447
Jakub Panek <jakub.panek@educanet.cz>
2021-09-14 12:31:06 +0200

    added

--- src/com/company/Atbas.java
+++ src/com/company/Atbas.java
@@ -7,35 +7,12 @@ public class Atbas {
         Scanner sc = new Scanner(System.in);
         System.out.println("Text k zasifrovani:");
         String text = sc.nextLine();
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
-        String finalText = text
-                .replace("a", "z")
-                .replace("b","y")
-                .replace("c","x")
-                .replace("d","w")
-                .replace("e","v")
-                .replace("f","u")
-                .replace("g","t")
-                .replace("h","s")
-                .replace("i","r")
-                .replace("j","q")
-                .replace("k","p")
-                .replace("l","o")
-                .replace("m","n")
-                .replace("n","m")
-                .replace("o","l")
-                .replace("p","k")
-                .replace("q","j")
-                .replace("r","i")
-                .replace("s","h")
-                .replace("t","g")
-                .replace("u","f")
-                .replace("v","e")
-                .replace("w","d")
-                .replace("x","c")
-                .replace("y","b")
-                .replace("z","a");
-
-        System.out.println(finalText.toLowerCase());
+        for (String word : letters) {
+            if (text.contains(word)) {
+                System.out.println(text.replace(word, "X"));
+            }
+        }
     }
 }
